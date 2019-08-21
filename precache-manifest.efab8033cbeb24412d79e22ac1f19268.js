4440af86e07fbb983ec058a0f94a36a4d56d610c
adamgao1996 <adamgao1996@gmail.com>
2019-08-20 22:33:37 -0400

    minor changes

---
+++ precache-manifest.efab8033cbeb24412d79e22ac1f19268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017d6368e4abb197e19fbdd1ca0f9260",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d32513fc92d2db386179",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "28f113873efeec4db13b",
+    "url": "/static/js/2.185b8862.chunk.js"
+  },
+  {
+    "revision": "d32513fc92d2db386179",
+    "url": "/static/js/main.f3780278.chunk.js"
+  },
+  {
+    "revision": "0f7195764353f1890297",
+    "url": "/static/js/runtime~main.424e23ca.js"
+  }
+]);
